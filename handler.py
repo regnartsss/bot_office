d40f60e827e3b657cc73c929a405ef230a13e22d
regnarts <regnartsss@gmail.com>
2023-12-25 16:27:52 +0700

    test

--- handler.py
+++ handler.py
@@ -9,14 +9,15 @@ router.message.filter(F.chat.type == "supergroup")
 
 @router.message(Command('name'))
 async def rename_admin(m: types.Message, command: CommandObject, bot: Bot):
-    await bot.send_message(m.chat.id, text="Hi, {}".format(command.args))
     try:
         await bot.promote_chat_member(chat_id=m.chat.id, user_id=m.from_user.id, can_manage_chat=True)
     except aiogram.exceptions.TelegramBadRequest:
         pass
-    try:
+    temp = await bot.get_chat_administrators(chat_id=m.chat.id)
+    print(temp[0].status)
+    if temp[0].status == 'administrator':
+        await bot.send_message(m.chat.id, text="Ваша роль в чате администратор, исправьте информацию о себе самостоятельно".format(command.args))
+    else:
         await bot.set_chat_administrator_custom_title(chat_id=m.chat.id,user_id=m.from_user.id, custom_title=command.args)
-    except aiogram.exceptions.TelegramBadRequest:
-        pass
-
+        await bot.send_message(m.chat.id, text="Приветствую, {}".format(command.args))
 
